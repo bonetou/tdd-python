c7b295b41cea875a69a569422da28725da2caae5
Bonetou <h.boneto@outlook.com>
2021-07-05 10:21:52 -0300

    feat: Criando um novo teste

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -23,3 +23,24 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_lance_esperado, avaliador.maior_lance)
         self.assertEqual(menor_lance_esperado, avaliador.menor_lance)
 
+    def test_avalia2(self):
+        henrique = Usuario('Henrique')
+        maicon = Usuario('Maicon')
+
+        lance_henrique = Lance(henrique, 100.0)
+        lance_maicon = Lance(maicon, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_maicon)
+        leilao.lances.append(lance_henrique)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        maior_lance_esperado = 150.0
+        menor_lance_esperado = 100.0
+
+        self.assertEqual(maior_lance_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_lance_esperado, avaliador.menor_lance)
+
