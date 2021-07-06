1db04c2f8c5b78b557f58d5b20fe78ceec4cce4f
Bonetou <h.boneto@outlook.com>
2021-07-06 11:01:09 -0300

    feat:  Estado e comportamento

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -16,8 +16,15 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
 
     def propoe(self, lance: Lance):
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
+
         self.__lances.append(lance)
 
     @property
@@ -25,15 +32,3 @@ class Leilao:
         # devolve uma cópia da lista, para não ter a mesma ref
         # assim, poderemos usar somente o método propoe
         return self.__lances[:] 
-        
-class Avaliador:
-    def __init__(self) -> None:
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
-
-    def avalia(self, leilao: Leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
