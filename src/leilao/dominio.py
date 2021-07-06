6f9c17769809e5848d4b00f8d71d902fc71f7319
Bonetou <h.boneto@outlook.com>
2021-07-06 12:13:43 -0300

    feat: Duas novas regras e novos testes

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -1,4 +1,6 @@
 import sys
+
+
 class Usuario:
     def __init__(self, nome):
         self.__nome = nome
@@ -7,11 +9,13 @@ class Usuario:
     def nome(self):
         return self.__nome
 
+
 class Lance:
     def __init__(self, usuario, valor):
         self.usuario = usuario
         self.valor = valor
-    
+
+
 class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
@@ -20,15 +24,17 @@ class Leilao:
         self.menor_lance = sys.float_info.max
 
     def propoe(self, lance: Lance):
-        if lance.valor > self.maior_lance:
-            self.maior_lance = lance.valor
-        if lance.valor < self.menor_lance:
-            self.menor_lance = lance.valor
 
-        self.__lances.append(lance)
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            if lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
+
+            self.__lances.append(lance)
 
     @property
     def lances(self):
         # devolve uma cópia da lista, para não ter a mesma ref
         # assim, poderemos usar somente o método propoe
-        return self.__lances[:] 
+        return self.__lances[:]
--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 from src.leilao.dominio import Usuario, Leilao, Lance
 
-class TestAvaliador(TestCase):
+
+class TestLeilao(TestCase):
 
     def setUp(self):
         self.henrique = Usuario('Henrique')
@@ -15,7 +16,6 @@ class TestAvaliador(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         self.leilao.propoe(self.lance_henrique)
         self.leilao.propoe(self.lance_maicon)
-
 
         maior_lance_esperado = 150.0
         menor_lance_esperado = 100.0
@@ -38,7 +38,7 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(150, self.leilao.menor_lance)
         self.assertEqual(150, self.leilao.maior_lance)
-        
+
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
         vini = Usuario('Vini')
         lance_do_vini = Lance(vini, 200.0)
@@ -52,3 +52,28 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_permitir_propor_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_henrique)
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebido)
+
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_for_diferente(self):
+        yuri = Usuario('Yuri')
+        lance_yuri = Lance(yuri, 200.)
+
+        self.leilao.propoe(self.lance_henrique)
+        self.leilao.propoe(lance_yuri)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(2, quantidade_de_lances_recebido)
+
+    def test_nao_deve_permitir_propor_o_lance_caso_o_ultimo_usuario_for_o_mesmo(self):
+        lance_henrique2 = Lance(self.henrique, 200.0)
+        self.leilao.propoe(self.lance_henrique)
+        self.leilao.propoe(lance_henrique2)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(1, quantidade_de_lances_recebido)
