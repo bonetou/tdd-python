99f0479f391f0ca0ca54721c8e2ad0a20f600fa2
Bonetou <h.boneto@outlook.com>
2021-07-08 15:56:32 -0300

    feat: Um pouco mais de testes

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -3,6 +3,24 @@ from src.leilao.dominio import Leilao, Usuario
 def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
     vini = Usuario('Vini', 100)
     leilao = Leilao('Celular')
-    vini.propoe_lance(leilao, 50.)
+    vini.propoe_lance(leilao, 50)
 
-    assert vini.carteira == 50.
+    assert vini.carteira == 50
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_menor_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 1)
+    assert vini.carteira == 99
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_igual_ao_valor_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 100)
+    assert vini.carteira == 0
+
+def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira():
+    vini = Usuario('Vini', 100)
+    leilao = Leilao('Celular')
+    vini.propoe_lance(leilao, 110)
+    assert vini.carteira == 100
